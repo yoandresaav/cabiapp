531a6454ed7f7f7d3302c7011b655395bd62d286
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2019-03-13 12:32:15 -0600

    add setup user & pass email

--- cabiapp/cabiapp/settings.py
+++ cabiapp/cabiapp/settings.py
@@ -167,3 +167,11 @@ LOGOUT_URL = 'logout'
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = False
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = config.get('email', 'EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config.get('email', 'EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 465
