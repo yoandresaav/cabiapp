448ac9097e8970889856439ebac31ed60be552fc
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2019-03-13 12:06:42 -0600

    add static root

--- cabiapp/cabiapp/settings.py
+++ cabiapp/cabiapp/settings.py
@@ -155,7 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = '/home/ubuntu/cabiapp/cabiapp/static/'
+if not DEBUG:
+    STATIC_ROOT = '/home/ubuntu/cabiapp/cabiapp/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
