1f861a6742ee0fa57fc862301bcf44d7be0f3d89
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2019-03-13 14:23:07 -0600

    modificate config key 1

--- cabiapp/cabiapp/settings.py
+++ cabiapp/cabiapp/settings.py
@@ -155,12 +155,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static")
+    ]
+else:
     STATIC_ROOT = '/home/ubuntu/cabiapp/cabiapp/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 LOGOUT_REDIRECT_URL = '/'
 LOGOUT_URL = 'logout'
