b72913b222756f382f163ba39df6f2f114410e82
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2019-03-13 20:16:28 -0600

    add recaptcha 3

--- cabiapp/cabiapp/settings.py
+++ cabiapp/cabiapp/settings.py
@@ -171,12 +171,13 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_SSL = False
+    EMAIL_USE_SSL = True
     EMAIL_USE_TLS = False
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = config.get('email', 'EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = config.get('email', 'EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = 465
+    #EMAIL_PORT = 465
+    EMAIL_PORT = 587
 
 RECAPTCHA_SECRET_KEY = config.get('recaptcha', 'RECAPTCHA_SECRET_KEY'),
 
