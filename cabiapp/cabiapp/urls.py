3d8408f64ddbd631090d6c84ea62dc55229f0b42
Yoandre Saavedra <yoandresaavedra@gmail.com>
2019-05-15 13:54:28 -0500

    add url

--- cabiapp/cabiapp/urls.py
+++ cabiapp/cabiapp/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 admin.site.site_title = 'Administración del Sitio CabiFleet'
 admin.site.site_header = 'Administración del Sitio CabiFleet'
@@ -9,6 +10,7 @@ admin.site.site_header = 'Administración del Sitio CabiFleet'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web_site.urls')),
+    path('google839fd1fe8fb8173c.html', TemplateView.as_view(template_name='google839fd1fe8fb8173c.html')),
     path('accounts/', include('django.contrib.auth.urls')), 
     path('profile/', include('profiles.urls')), 
 ]
